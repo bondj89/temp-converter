08f14c3faba7a04a0f24c1967edccaf59ddcca6c
Bond <jobocuban@gmail.com>
2019-09-17 16:46:57 -0600

    Added a scanner loop for multiple conversions.

---
+++ edu/cnm/deepdive/TempCoverter.java
@@ -0,0 +1,53 @@
+package edu.cnm.deepdive;
+
+import java.util.*;
+
+public class TempCoverter {
+
+  private static Scanner input = new Scanner(System.in);
+
+  public static void main(String[] args) {
+      convert(input);
+
+  }
+
+
+  private static void convert(Scanner scanner) {
+    String line;
+    while (!(line = scanner.nextLine().trim()).isEmpty()) {
+      char finalChar = line.toUpperCase().charAt(line.length() - 1);
+      if (finalChar == 'C') {
+        double celsius = new ParseTemperature(line).invoke();
+        System.out.printf("%.3fF%n", celsiusToFahrenheit(celsius));
+      } else if (finalChar == 'F') {
+        double fahrenheit = new ParseTemperature(line).invoke();
+        System.out.printf("%.3fC%n", fahrenheitToCelsius(fahrenheit));
+      }
+    }
+
+  }
+
+  public static double celsiusToFahrenheit(double celsius) {
+    return 9 * celsius / 5 + 32;
+
+  }
+
+  public static double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+
+
+  private static class ParseTemperature {
+
+    private String line;
+
+    public ParseTemperature(String line) {
+      this.line = line;
+    }
+
+    public double invoke() {
+      line = line.substring(0, line.length() - 1).trim();
+      return Double.parseDouble(line);
+    }
+  }
+}
